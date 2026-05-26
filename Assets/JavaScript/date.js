6e28472b40ebc342c8b178c323b1b15733d1638f
Drohenxx <amencris49@gmail.com>
2026-05-26 14:04:01 -0400

    update

--- Assets/JavaScript/date.js
+++ Assets/JavaScript/date.js
@@ -1,4 +1,9 @@
 
 let hora_actual = new Date();
+let ano_actual = hora_actual.getFullYear();
 
 document.querySelector(".hora").textContent = (hora_actual)
+
+document.querySelector("#year").textContent = (ano_actual)
+
+
